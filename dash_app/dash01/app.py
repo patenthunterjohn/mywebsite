80cd0096124cf8416403c25aad845dbed34e0bea
patenthunterjohn <patenthunterjohn.ai@gmail.com>
2025-05-04 14:12:02 +0800

    Update app.py

--- dash_app/dash01/app.py
+++ dash_app/dash01/app.py
@@ -1,5 +1,6 @@
 import dash
 from dash import html
+import os
 
 # 初始化 Dash 應用
 app = dash.Dash(__name__)
@@ -10,7 +11,6 @@ app.layout = html.Div([
     html.P("這是部署在 Render 的 Dash 應用程式。")
 ])
 
-# 使用 Render 自動分配的端口
+# 這段程式碼可以省略
 if __name__ == "__main__":
     app.run_server(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 10000)))
-    # os.environ.get("PORT", 10000) 會首先從環境變數中讀取 PORT，如果沒有設置，則使用預設的端口 10000。這樣無論在哪個環境中運行，都能使用 Render 提供的端口。
